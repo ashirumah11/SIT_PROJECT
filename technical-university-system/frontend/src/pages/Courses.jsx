99877476257a65e3dfc75574ea96dd960e3a5875
ashirumah11 <josephanyangu1@gmail.com>
2026-09-11 15:29:28 +0300

    Fix Vite build errors

--- technical-university-system/frontend/src/pages/Courses.jsx
+++ technical-university-system/frontend/src/pages/Courses.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import CourseCard from '../components/CourseCard.jsx'
 import engineeringImage from '../assets/course-engineering1.jpg'
-import fashionImage from '../assets/course-fashion1.jpg'
+import fashionImage from '../assets/course-fashion1.JPG'
 import hospitalityImage from '../assets/course-hospitality1.jpg'
 import ictImage from '../assets/course-ict.jpg'
 import cosmetologyImage from '../assets/course-hairdressing.jpg'
